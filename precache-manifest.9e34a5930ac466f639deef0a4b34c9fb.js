368c56b8d65d1c832580e7a8fc99f05ced6f9618
waltermaffy <38728429+waltermaffy@users.noreply.github.com>
2021-11-24 23:22:56 +0000

    Deploying to gh-pages from  @ 2656894dfba93382418b59b821589e1c2abeaebc 🚀

---
+++ precache-manifest.9e34a5930ac466f639deef0a4b34c9fb.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1002f3f6a4800cbc5e88e1c883942c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f067e83db6d15d7eddc",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "fc743e1cf3d5b9715e96",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "f8bef7a8a86f98135c1c",
+    "url": "/static/css/main.66262938.chunk.css"
+  },
+  {
+    "revision": "07add35b38f0e4680d3b",
+    "url": "/static/js/2.92ee6538.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.92ee6538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f067e83db6d15d7eddc",
+    "url": "/static/js/3.6717dbd6.chunk.js"
+  },
+  {
+    "revision": "fc743e1cf3d5b9715e96",
+    "url": "/static/js/4.20b3dc7c.chunk.js"
+  },
+  {
+    "revision": "f8bef7a8a86f98135c1c",
+    "url": "/static/js/main.7fe45963.chunk.js"
+  },
+  {
+    "revision": "2dde4737baf2b0838f23",
+    "url": "/static/js/runtime-main.b165f3de.js"
+  },
+  {
+    "revision": "8b28a4bee540585a929d151cb37baeb0",
+    "url": "/static/media/ADF_w.8b28a4be.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "92d283c02732da8a3eb91e487770ab4c",
+    "url": "/static/media/polito.92d283c0.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
